1570f7f02e3ce36485804767ccfa57670f8ef3f8
Daniel Slupskiy <slupdo09@gmail.com>
2017-04-16 21:16:12 +0300

    Initial Commit

---
+++ AppleRssApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleRssApp
+//
+//  Created by Ivashin Dmitry on 4/16/17.
+//  Copyright © 2017 Ivashin Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
